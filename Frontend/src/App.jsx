c17e5b065f79034078bb9ff877a02e0f601f1701
khushibansal13 <khushibansal1302@gmail.com>
2023-08-26 03:56:18 +0530

    harshi

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,23 @@
 import { useState } from 'react'
-import './App.css'
+// import './App.css'
 import {BrowserRouter as Router , Routes , Route , Link} from 'react-router-dom'
 import Reception from './pages/Reception'
 function App() {
 
   return (
-    <>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Reception />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </Router>
+      <div>
+        <div>logo</div>
+        <div className='flex justify-between hover:text-'>
+          <div>welcome to healthsync</div>
+          <div>
+            <img src="https://healthcarentsickcare.com/cdn/shop/articles/what-is-healthcare-707998.png?v=1677580538">
+            </img>
+          </div>
+        </div>
+      </div>
 
-    </>
   )
+        
 }
 
 export default App
