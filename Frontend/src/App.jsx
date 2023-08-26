94c195c611dcd3710ae54e385b217bfc71eb94d4
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2023-08-26 06:20:13 +0530

    commited

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Reception from "./pages/Reception";
+import Nurse from "./pages/Nurse";
 // import ReceptionLogin from "./pages/receptionLogin"
 
 function App() {
@@ -10,10 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Reception />} />
-<<<<<<< HEAD
-          {/* <Route path="/reception" element={<ReceptionLogin />} /> */}
-=======
->>>>>>> refs/remotes/origin/main
+          <Route path="/nurse" element={<Nurse />} />
         </Routes>
       </Router>
     </>
