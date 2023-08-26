701cab9b5f42c009b456d074e0458727292e6182
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2023-08-26 06:57:09 +0530

    Card

--- Frontend/src/pages/Nurse.jsx
+++ Frontend/src/pages/Nurse.jsx
@@ -1,9 +1,21 @@
 import React from "react";
-import Card from "../comoonents/Card";
+import Card from "../components/Card";
+import {Button,FormControlLabel,FormGroup  } from "@mui/material"
+
 export default function Nurse() {
+
+    getPatientdata = () => {
+        fetch("http://localhost:3000/api/patient")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+
   return (
-    <div>
-      <Card />
+    <div >
+        <Button variant="contained" onClick={getPatientdata}>
+            <Card />
+
+        </Button>
     </div>
   );
 }
